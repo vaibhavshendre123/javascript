139f267c261cb9eebcb186c6a73e0d26938fc1a9
vaibhavshendre123 <vaibhavshendre58@gmail.com>
2026-02-18 01:23:50 -0800

    Add files via upload

---
+++ vscode/variables.js
@@ -0,0 +1,8 @@
+var a = "Hello";
+var _a = "Hi";
+var $a = "Welcome to Javascript";
+console.log(a , _a , $a );
+let ab = "Variable using let";
+const pi = 3.14;
+console.log(ab);
+console.log("Constant using const: " , pi);
